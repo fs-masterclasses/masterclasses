85a50535a905ac9f4ae4fc6c99cf74cefd094bbe
Miriam Forner <mforner13@gmail.com>
2020-04-23 22:04:11 +0100

    Test create masterclass content routes

--- tests/test_masterclasses.py
+++ tests/test_masterclasses.py
@@ -1,8 +1,11 @@
 import pytest
 
+from flask import template_rendered, session, url_for
+from contextlib import contextmanager
 from app import create_app
 from app import db as _db
 from config import *
+from app.models import MasterclassContent
 
 @pytest.fixture(scope="session")
 def test_app():
@@ -19,7 +22,6 @@ def test_client(test_app):
     # Establish an application context before running the tests.
     ctx = test_app.test_request_context()
     ctx.push()
-    session["candidate-id"] = 1
 
     yield testing_client  # this is where the testing happens!
 
@@ -47,6 +49,15 @@ def db(test_client):
     _db.drop_all()
 
 
+@pytest.fixture
+def new_masterclass_content_data_category(db):
+    mc = MasterclassContent(name='Introduction to R', description='This masterclass will give you a solid understanding of how to run queries using R.', category='Data')
+    db.session.add(mc)
+    db.session.commit()
+    yield
+    db.session.delete(mc)
+    db.session.commit()
+
 def test_login_not_required(test_client, db):
     rv = test_client.get('/')
     assert rv.status_code == 200
@@ -55,3 +66,38 @@ def test_login_not_required(test_client, db):
 def test_login_required(logged_in_user, test_client):
     rv = test_client.get('/')
     assert rv.status_code == 200
+
+@contextmanager
+def captured_templates(app):
+    recorded = []
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+        print('hereeeeee', context)
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+@pytest.mark.parametrize('endpoint, data, expected_template, expected_status_code',
+(
+    ('/choose-content', {'select-job-family': 'Data'}, 'create-masterclass/content/new-or-existing.html', 200),
+    ('/choose-content/new-or-existing', {'which-masterclass': 'new masterclass'}, 'create-masterclass/content/create-new.html', 200),
+))
+def test_user_is_displayed_the_correct_template_according_to_request(test_app, test_client, logged_in_user, new_masterclass_content_data_category, endpoint, data, expected_template, expected_status_code):
+    with captured_templates(test_app) as templates:
+        response = test_client.post(endpoint, data = data)
+    assert response.status_code == expected_status_code
+    assert len(templates) == 1
+    template, context = templates[0]
+    assert template.name == expected_template
+
+@pytest.mark.parametrize('endpoint, data, expected_route',
+(
+('/choose-content/new-or-existing', {'which-masterclass': 'Introduction to R'}, 'main_bp.index'),
+('/choose-content/create-new', {'masterclass-name': 'A name', 'masterclass-description': 'A description'}, 'main_bp.index'),
+))
+def test_user_is_redirected_to_correct_url(test_client, logged_in_user, new_masterclass_content_data_category, endpoint, data, expected_route):
+    response = test_client.post(endpoint, data = data)
+    assert response.status_code == 302
+    assert response.location == f'http://localhost{url_for(expected_route)}'
