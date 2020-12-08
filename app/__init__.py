6083ca9a61e41830dd63587b04a3c9db4ca6021f
Miriam Forner <mforner13@gmail.com>
2020-12-08 21:31:09 +0000

    Add google maps client with API key

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import googlemaps
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'main_bp.login'
+gmaps = googlemaps.Client(key=os.environ.get("GOOGLE_MAPS_API_KEY", default='AIza_KEY_HERE'))
 
 
 def create_app(config_class=Config):
